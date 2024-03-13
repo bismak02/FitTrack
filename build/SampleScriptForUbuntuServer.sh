b6b358a6611d5422e18383023612b20fc11007fd
Martin Poblete <martinpoblete9166@outlook.com>
2024-03-13 07:44:04 -0500

    Create a sample script for post install of the ubuntu front end server

---
+++ build/SampleScriptForUbuntuServer.sh
@@ -0,0 +1,42 @@
+# Sample script for ubuntu server
+
+# these are commands for debugging
+# set -e will exit if a command exits with a non zero status, like an error
+# set -v will print commands as they are executed in the script which should help debugging
+set -e
+set -v
+
+sudo apt-get update
+sudo apt install nginx
+sudo apt update
+sudo apt install git -y
+
+cd ~
+
+# Need to either have the config file in the image already or create in the script
+# Same for the github key either it has to be in the image or we have to find another way to insert it, for example using vault
+git clone git@github.com:illinoistech-itm/mpoblete.git
+
+# incase there are access errors
+# sudo chmod a+x /home /home/vagrant /home/vagrant/team01o-2024 /home/vagrant/team01o-2024/design /home/vagrant/team01o-2024/design/code
+
+# need to edit the sites-available file 
+cd /etc/nginx/sites-available/
+sudo vim team01o_website.conf
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+sudo ln -s /etc/nginx/sites-available/team01o_website.conf /etc/nginx/sites-enabled/
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --reload
+sudo firewall-cmd --reload
+sudo firewall-cmd --zone=public --list-all
+
+sudo nginx -t
+sudo systemctl reload nginx
+
