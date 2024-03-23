6579e1f74da697b39b2e25227c962c6289d88f3c
Martin Poblete <martinpoblete9166@outlook.com>
2024-03-23 16:07:54 -0500

    Add comments to each table to guide on what needs to be added

--- code/database/create-tables.sql
+++ code/database/create-tables.sql
@@ -8,6 +8,7 @@ FullName
 Username
 Email
 Password
+
 Exercise-Log Table: Here, we log the exercises performed by each user.
 
 ExerciseLogID (Primary Key)
@@ -16,16 +17,19 @@ IntensityID (Foreign Key)
 ExerciseID (Foreign Key)
 Duration (In minutes)
 Date
+
 Intensity Table: This table defines the intensity levels for exercises.
 
 IntensityID (Primary Key)
 Intensity (e.g., Low, Medium, High)
 BurnMultiplier
+
 Exercises Table: Contains a list of exercises and their calorie burn rates.
 
 ExerciseID (Primary Key)
 Exercise
 CaloriesBurnt (Calories burnt per minute)
+
 Nutrition-Log Table: This table records the food items consumed by the user.
 
 NutritionLogID (Primary Key)
@@ -38,6 +42,7 @@ Food Table: Contains information about various food items and their calorie cont
 FoodID (Primary Key)
 FoodItem
 Calories (Per single food item)
+
 Goal Table: Lists the goals that users can set for themselves.
 
 GoalID (Primary Key)
@@ -45,6 +50,7 @@ GoalType (e.g., Exercise/Nutrition)
 Target (Specific target for the goal)
 Description
 Difficulty (e.g., Easy, Medium, Hard)
+
 UserGoals Table: Associates users with their selected goals and tracks the status of each.
 
 UserGoalID (Primary Key)
@@ -54,6 +60,7 @@ Status (e.g., In Progress, Completed)
 DateCreated
 LastUpdated
 DateCompleted
+
 GoalProgress Table: Logs the progress users make towards their goals.
 
 GoalProgressID (Primary Key)
---
+++ code/database/insert-exercise-records.sql
@@ -0,0 +1,17 @@
+/*
+
+Need to generate exercises to be inserted into the exercise table
+
+Exercises Table: Contains a list of exercises and their calorie burn rates.
+
+ExerciseID (Primary Key)
+Exercise
+CaloriesBurnt (Calories burnt per 15 minutes at average intensity)
+
+Example:
+
+ExerciseID: 1
+Exercise: Push-ups
+CaloriesBurnt: 80
+
+*/
