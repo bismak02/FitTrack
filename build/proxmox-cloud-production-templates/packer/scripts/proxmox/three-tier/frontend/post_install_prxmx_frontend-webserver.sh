dca0964b08743fde3c438cba0c33de4b7b580b56
Martin Poblete <martinpoblete9166@outlook.com>
2024-04-21 09:21:32 -0500

    Make sure the necessary commands are ran as vagrant

--- build/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_frontend-webserver.sh
+++ build/proxmox-cloud-production-templates/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_frontend-webserver.sh
@@ -10,16 +10,16 @@ sudo apt-get install -y curl rsync
 # https://github.com/nodesource/distributions/blob/master/README.md#using-ubuntu-2
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm install -g npm@9.6.0
+sudo -u vagrant npm install -g npm@9.6.0
 
 # Change directory to the location of your JS code
 cd /home/vagrant/team01o-2024/design/code/client/
 
 # Downloading necessary react scripts
-sudo npm install react-scripts
+sudo -u vagrant npm install react-scripts
 
 # Making an optimized build of the react app
-sudo npm run build
+sudo -u vagrant npm run build
 
 # Installing pm2 to keep our server running
 sudo npm install pm2@latest -g
@@ -28,7 +28,7 @@ sudo npm install pm2@latest -g
 sudo npm install -g serve
 
 # Running our server with pm2 and serve
-pm2 serve build 3000 --spa
+sudo -u vagrant pm2 serve build 3000 --spa
 
 # Saving this setup so pm2 runs on startup
 sudo -u vagrant pm2 startup
